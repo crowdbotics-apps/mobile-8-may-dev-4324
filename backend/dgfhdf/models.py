652f62e132a88389f82bc2054ff404bb0735399d
Crowdbotics <team@crowdbotics.com>
2020-05-08 12:32:34 +0000

    Update models

--- backend/dgfhdf/models.py
+++ backend/dgfhdf/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Ffgjy(models.Model):
     "Generated Model"
     srdyetg = models.BigIntegerField()
+    test = models.PositiveSmallIntegerField(null=True, blank=True,)
 
 
 # Create your models here.
